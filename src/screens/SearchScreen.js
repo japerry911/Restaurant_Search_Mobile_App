64697472fc623184ca53d03e4c18343adbc3a4e0
japerry911 <jack.e.perry@outlook.com>
2020-03-29 18:55:07 -0600

    .env file added!! And react-native-dotenv works in retrieving env variables

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [restaurants, setRestaurants] = useState([]);
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term,
+                location: 'san jose'
+            }
+        });
+
+        setRestaurants(response.data.businesses);
+    }
 
     return (
         <View>
             <SearchBar 
                 term={term} 
-                onTermChange={newTerm => setTerm(newTerm)} 
-                onTermSubmit={() => console.log('term was submitted')}
+                onTermChange={setTerm} 
+                onTermSubmit={searchApi}
             />
             <Text>Search Screen!</Text>
-            <Text>{term}</Text>
+            <Text>We have found {restaurants.length} results</Text>
         </View>
     );
 }
